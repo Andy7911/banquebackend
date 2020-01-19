f6135156ae662e3505b76463ea416d032cee495d
andy12425 <andersonbeauvais1@hotmail.fr>
2020-01-19 00:26:41 -0500

    autorisation ajouter delete.php

---
+++ client/delete.php
@@ -0,0 +1,17 @@
+<?php 
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header("Access-Control-Allow-Credentials, true");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods, DELETE, POST, GET, OPTIONS");
+header("Access-Control-Max-Age: 86400");
+header("Access-Control-Expose-Headers: Access-Control-Allow-Origin");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+
+
+
+
+
+
+?>
